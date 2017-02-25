5495c1eb2c2fcc337125595730d04ffb2ce4d93b
abruckman <abruckman09@gmail.com>
2017-02-24 16:40:28 -0800

    added tests

--- botofyou.rb
+++ botofyou.rb
@@ -1,51 +1,5 @@
-require 'rubygems'
-require 'dotenv'
-require 'json'
-require 'twitter'
-require 'markov_chains'
+require_relative 'make_robots.rb'
 
-Dotenv.load
-
-$streaming_client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = ENV["CONSUMER_KEY"]
-  config.consumer_secret     = ENV["CONSUMER_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-end
-
-$rest_client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["CONSUMER_KEY"]
-  config.consumer_secret     = ENV["CONSUMER_SECRET"]
-  config.access_token        = ENV["ACCESS_TOKEN"]
-  config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-end
-
-
-def create_library(handle)
-  user_tweets = []
-
-  api_return = $rest_client.user_timeline(screen_name: handle, include_rts: false, count: 1000)
-  api_return.each do |tweet|
-     user_tweets << tweet.text
-  end
-  p user_tweets.length
-  p handle
-
-  library = user_tweets.join(" ").gsub(/http\S*\s/, "")
-  library
-end
-
-def new_tweet(library, target_user)
-    generator =MarkovChains::Generator.new(library)
-    tweet = ''
-    until tweet.length.between?(90,120)
-      tweet = generator.get_sentences(1)
-      tweet = tweet[0]
-    end
-    tweet = ".@#{target_user} " + tweet
-  end
-
-p new_tweet(create_library("cushbomb"), "botofyou")
 
 loop do
   $streaming_client.user do |object|
---
+++ make_robots_spec.rb
@@ -0,0 +1,22 @@
+require 'rspec'
+require_relative 'make_robots.rb'
+
+describe "twitter client works" do
+  it "makes a call to the twitter API" do
+    expect($rest_client.user_timeline(screen_name: "dril", include_rts: false, count: 1000)).to include(a_kind_of(Twitter::Tweet))
+  end
+
+end
+
+describe "makes tweets for user " do
+  let (:library){create_library("dril")}
+  it "makes turns user timeline into a markov_chains library" do
+    expect(create_library("dril")).to be_a_kind_of(String)
+  end
+  it "uses the library to make a tweet" do
+    expect(new_tweet(library, "dril")).to be_a_kind_of(String)
+
+    expect(new_tweet(library, "dril").length).to be_between(90, 140)
+  end
+
+end
