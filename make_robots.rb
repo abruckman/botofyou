24e4b966f17c9c0695190bf004f90b838595f3ab
abruckman <abruckman09@gmail.com>
2017-02-24 16:17:55 -0800

    bot works, tests slow

--- make_robots.rb
+++ make_robots.rb
@@ -46,28 +46,3 @@ def new_tweet(library, target_user)
   end
 
 p new_tweet(create_library("cushbomb"), "botofyou")
-
-loop do
-  $streaming_client.user do |object|
-    case object
-      when Twitter::Tweet
-        if object.user == $rest_client.user
-          p "this is a repeat"
-        else
-          library = create_library(object.user.screen_name)
-          2.times do
-            reply_content = new_tweet(library, object.user.screen_name)
-            reply_id = object.id
-            p reply_content
-
-            $rest_client.update(reply_content, in_reply_to_status_id: reply_id)
-          end
-        end
-        # we are very close here also IDK about this gem
-      when Twitter::DirectMessage
-        puts "It's a direct message!"
-      when Twitter::Streaming::StallWarning
-        warn "Falling behind!"
-    end
-  end
-end
