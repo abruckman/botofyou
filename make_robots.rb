3dcbe4d74e9f99ab358eb9dc14b23e29ab27be6b
abruckman <abruckman09@gmail.com>
2017-04-10 15:57:27 -0700

    changed test case

--- make_robots.rb
+++ make_robots.rb
@@ -91,9 +91,9 @@ def check_fails(library, target_user)
 end
 
 
-check_library = create_library("cushbomb")
+check_library = create_library("dril")
 
-check_fails(check_library, "cushbomb")
+check_fails(check_library, "dril")
 3.times do
-  p new_tweet(create_library("cushbomb"), "cushbomb")
+  p new_tweet(create_library("dril"), "dril")
 end
